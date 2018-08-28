f2ae0034a36f163d86bd16e96868040cf8321d1f
Christoph Prybila <christoph.prybila@dkb.com>
2018-08-28 09:24:22 +0200

    Added starting point

--- src/main/java/com/senacor/bankathon2018/pocloyServiceApplication.java
+++ src/main/java/com/senacor/bankathon2018/pocloyServiceApplication.java
@@ -1,5 +1,13 @@
 package com.senacor.bankathon2018;
 
-public class pocloyServiceApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocloyServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PocloyServiceApplication.class, args);
+  }
 
 }
