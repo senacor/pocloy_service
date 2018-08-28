4a230fb201f2e811c6098a0dea0a1bcda302d624
Christoph Prybila <christoph.prybila@dkb.com>
2018-08-28 22:24:05 +0200

    Added clearDB service

--- src/main/java/com/senacor/bankathon2018/service/DemoDataService.java
+++ src/main/java/com/senacor/bankathon2018/service/DemoDataService.java
@@ -30,10 +30,17 @@ public class DemoDataService {
         ImmutableMap.of(LoyaltyContent.sunglasses, 5, LoyaltyContent.white_balance_sunny, 10)));
     vouchers.add(new Voucher(4, "10L of Petrol",
         ImmutableMap.of(LoyaltyContent.gas_station, 5, LoyaltyContent.car_hatchback, 5)));
-    vouchers.add(new Voucher(5, "Burger",
-        ImmutableMap.of(LoyaltyContent.hamburger, 1)));
-    vouchers.add(new Voucher(6, "10L of Petrol",
-        ImmutableMap.of(LoyaltyContent.pizza, 1)));
+    vouchers.add(new Voucher(5, "Hamburger", ImmutableMap.of(LoyaltyContent.hamburger, 1)));
+    vouchers.add(new Voucher(6, "Wein", ImmutableMap.of(LoyaltyContent.bottle_wine, 1)));
+    vouchers.add(new Voucher(7, "Auto", ImmutableMap.of(LoyaltyContent.car_hatchback, 1)));
+    vouchers.add(new Voucher(8, "Cup", ImmutableMap.of(LoyaltyContent.cup, 1)));
+    vouchers.add(new Voucher(9, "Apple", ImmutableMap.of(LoyaltyContent.food_apple, 1)));
+    vouchers.add(new Voucher(10, "Food", ImmutableMap.of(LoyaltyContent.food, 1)));
+    vouchers.add(new Voucher(11, "Gas", ImmutableMap.of(LoyaltyContent.gas_station, 1)));
+    vouchers.add(
+        new Voucher(12, "Silverware", ImmutableMap.of(LoyaltyContent.silverware_fork_knife, 1)));
+    vouchers.add(new Voucher(13, "Sunglasses", ImmutableMap.of(LoyaltyContent.sunglasses, 1)));
+    vouchers.add(new Voucher(14, "Sun", ImmutableMap.of(LoyaltyContent.white_balance_sunny, 1)));
   }
 
   public List<Voucher> getVouchers() {
--- src/main/java/com/senacor/bankathon2018/webendpoint/User.java
+++ src/main/java/com/senacor/bankathon2018/webendpoint/User.java
@@ -121,4 +121,5 @@ public class User {
                 .body(errorMsg);
         }
     }
+
 }
