ab4a4db6a08ad80b8eb2007ddcecba7ae5a9d554
Christoph Prybila <christoph.prybila@dkb.com>
2018-08-28 11:38:38 +0200

    added transaction endpoint

--- src/main/java/com/senacor/bankathon2018/webendpoint/User.java
+++ src/main/java/com/senacor/bankathon2018/webendpoint/User.java
@@ -1,6 +1,14 @@
 package com.senacor.bankathon2018.webendpoint;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.senacor.bankathon2018.dto.LoyaltyCode;
+import com.senacor.bankathon2018.dto.LoyaltyContent;
+import com.senacor.bankathon2018.dto.LoyaltyStatus;
 import com.senacor.bankathon2018.webendpoint.model.Credentials;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,4 +21,26 @@ public class User {
     public ResponseEntity<Void> login(@RequestBody Credentials credentials) {
         return ResponseEntity.ok().build();
     }
+
+  @PostMapping("/transactions")
+  public String transactions() throws JsonProcessingException {
+    LoyaltyCode loyaltyCode = new LoyaltyCode(
+        "ae546c90-c381-4e73-b38d-0565f1200b94",
+        LoyaltyStatus.packed.toString(),
+        LoyaltyContent.unknown.toString(),
+        Calendar.getInstance().getTime());
+
+    LoyaltyCode loyaltyCodeUnpacked = new LoyaltyCode(
+        "96e86953-c71a-40f0-af60-5bd83d7a77c0",
+        LoyaltyStatus.unpacked.toString(),
+        LoyaltyContent.sun.toString(),
+        Calendar.getInstance().getTime());
+
+    List<LoyaltyCode> codes = new ArrayList<>();
+    codes.add(loyaltyCode);
+    codes.add(loyaltyCodeUnpacked);
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    return objectMapper.writeValueAsString(codes);
+  }
 }
